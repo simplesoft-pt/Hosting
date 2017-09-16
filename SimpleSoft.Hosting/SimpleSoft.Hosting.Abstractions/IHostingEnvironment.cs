4c6f68e49be8f5b48e3ccaaed19e681c7cf78a7a
João Simões <nogravity00@outlook.com>
2017-09-17 00:55:17 +0100

    Interface to aggregate hosting information

---
+++ SimpleSoft.Hosting/SimpleSoft.Hosting.Abstractions/IHostingEnvironment.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace SimpleSoft.Hosting
+{
+    /// <summary>
+    /// Provides information about the hosting environment in which the 
+    /// application is running.
+    /// </summary>
+    public interface IHostingEnvironment
+    {
+        /// <summary>
+        /// The application name.
+        /// </summary>
+        string ApplicationName { get; set; }
+
+        /// <summary>
+        /// The <see cref="IFileProvider"/> pointing at <see cref="ContentRootPath"/>.
+        /// </summary>
+        IFileProvider ContentRootFileProvider { get; set; }
+
+        /// <summary>
+        /// The absolute path to the directory that contains the
+        /// application files.
+        /// </summary>
+        string ContentRootPath { get; set; }
+
+        /// <summary>
+        /// The environment name.
+        /// </summary>
+        string Name { get; set; }
+    }
+}
