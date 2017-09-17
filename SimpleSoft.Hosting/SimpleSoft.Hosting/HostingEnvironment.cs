c4fff92e8c39237a73f5ff8399fd80bbfbea531a
João Simões <nogravity00@outlook.com>
2017-09-17 01:01:40 +0100

    Implementation for hosting environment

---
+++ SimpleSoft.Hosting/SimpleSoft.Hosting/HostingEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace SimpleSoft.Hosting
+{
+    /// <inheritdoc />
+    public class HostingEnvironment : IHostingEnvironment
+    {
+        /// <inheritdoc />
+        public string ApplicationName { get; set; }
+
+        /// <inheritdoc />
+        public IFileProvider ContentRootFileProvider { get; set; }
+
+        /// <inheritdoc />
+        public string ContentRootPath { get; set; }
+
+        /// <inheritdoc />
+        public string Name { get; set; }
+    }
+}
