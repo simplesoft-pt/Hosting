26d1d93faf2b493d932d343e5925da972f69389f
João Simões <nogravity00@outlook.com>
2017-10-02 16:48:56 +0100

    Release 1.0.0-rc03-final

--- Tools/UpdateAllVersions.ps1
+++ Tools/UpdateAllVersions.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 $assemblyVersion = "1.0.0"
-$assemblyFileVersion = "1.0.0.17263"
-$assemblyInformationalVersion = "1.0.0-rc02"
+$assemblyFileVersion = "1.0.0.17275"
+$assemblyInformationalVersion = "1.0.0-rc03-final"
 
 Write-Host "Updating project versions..."
 
